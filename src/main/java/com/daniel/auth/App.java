5e65cef027398e06755209ef5bea3c58a9fb6bc3
Daniel Shepherd <darts100@gmail.com>
2022-05-03 15:16:07 +1200

    Added username creation code

--- src/main/java/com/daniel/auth/App.java
+++ src/main/java/com/daniel/auth/App.java
@@ -1,15 +1,17 @@
 package com.daniel.auth;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Duration;
 
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 public class App {
+    private enum USERNAME_STATUS { UNAVAILABLE, INVALID, VALID }
+    private enum PASSWORD_STATUS { INVALID_COMMON, INVALID_SHORT, VALID }
     public static final int USERNAME_MAX = 20;
     public static final int PASSWORD_MAX = 64;
-    public static final int SALT_SIZE = 8; // 64 bit
     public static final String STORAGE_FILE_NAME = "Credentials.csv";
 
 
@@ -106,8 +108,33 @@ public class App {
                 // TODO: Proper error messages
                 e.printStackTrace();
             }
-
         }
+
+        try {
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private USERNAME_STATUS validateUsername(String username) {
+        // check if username is too short
+        if(username.length() < 3) {
+            return USERNAME_STATUS.INVALID;
+        }
+
+        // check if username already exists
+        if(Controller.findUsername(username)) {
+            return USERNAME_STATUS.UNAVAILABLE;
+        }
+
+        // TODO check for profanity
+        if(false) {
+            return USERNAME_STATUS.INVALID;
+        }
+
+        return USERNAME_STATUS.VALID;
     }
 
     private void login() {
@@ -117,8 +144,39 @@ public class App {
 
     private void registerAccount() {
         // TODO: Implement
-        boolean res = false;
-        System.out.println("Did insert succeed?: " + res);
+        boolean invalidUsername = true;
+        boolean invalidPassword = true;
+        try{
+            String username = "";
+            String password= "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while(invalidUsername) {
+                System.out.println("\n\nPlease enter a username (3 or more characters, no profanity):");
+                username = reader.readLine();
+
+                USERNAME_STATUS status = validateUsername(username);
+                switch (status) {
+                    case UNAVAILABLE:
+                        System.out.println("Username " + username + " is unavailable.");
+                        break;
+                    case INVALID:
+                        System.out.println("Username " + " is not valid.");
+                        break;
+                    case VALID:
+                        invalidUsername = false;
+                        break;
+                }
+            }
+
+            while(invalidPassword) {
+                System.out.println("\n\nPlease enter a password, must be at least 8 characters long:");
+                password = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void invalidInput() {
@@ -134,8 +192,12 @@ public class App {
 
     public static void main(String[] args) {
 	    System.out.println("Starting...");
-//        new App().go(args);
-        new App().testingArgon2();
+        // TODO: Get rid of testing
+        if(args.length == 0) {
+            new App().go(args);
+        } else {
+            new App().testingArgon2();
+        }
         System.out.println("Exiting...");
     }
 }
